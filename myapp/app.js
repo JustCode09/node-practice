1a57f5f6b9c9ae9a5bb152b72b6be54c5cea09b5
JustCode09 <Professionalacc011@gmail.com>
2025-08-01 13:38:36 +0545

    leanred bout modules here

---
+++ myapp/app.js
@@ -0,0 +1,6 @@
+const calculator = require('./calc');
+
+let x = 10;
+let y = 5;
+console.log('Add:', calculator.add(x, y));
+console.log('Subtract:', calculator.subtract(x, y));
---
+++ myapp/main.js
@@ -0,0 +1,5 @@
+const f = require('./func');
+
+console.log('Add:', f.add(5, 10));
+console.log('Subtract:', f.subtract(10, 5));
+
