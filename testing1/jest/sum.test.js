be6161e4fdf8b482b54fee481cb8556f015d1a20
JustCode09 <Professionalacc011@gmail.com>
2025-08-03 16:29:49 +0545

    learned about jest

--- testing1/jest/sum.test.js
+++ testing1/jest/sum.test.js
@@ -1,5 +1,17 @@
-const add = require('../sum');
+// const add = require('../sum');
 
-test('adds numbers', ()=>{
-    expect(add()).toBe(5);
-})
+// test('adds numbers', ()=>{
+//     expect(add()).toBe(5);
+// })
+
+// const sum = require('../sum');
+
+// test('adds 2+3 to equals 5', ()=>{
+//     expect(sum(2,3)).toBe(5);
+// })
+
+const sum = require('../sum');  
+
+test('retuens object ', ()=>{
+    expect(sum()).toEqual({ name: 'saurav' });
+});
